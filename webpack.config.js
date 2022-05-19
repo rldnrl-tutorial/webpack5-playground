d4bf9d9f53bfef7e03607b777ce8bb98725eb422
rldnrl <devpark1002@gmail.com>
2022-05-19 21:51:46 +0900

    feat: image 빌드 결과를 images로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: resolve(__dirname, 'dist')
+    path: resolve(__dirname, 'dist'),
+    publicPath: 'dist',
+    assetModuleFilename: 'images/[hash][ext][query]'
   },
   module: {
     rules: [
       {
         test: /.(png|jpg)$/,
-        type: 'asset/resource'
+        type: 'asset',
       }
     ],
   },
