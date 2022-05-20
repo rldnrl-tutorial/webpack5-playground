4edf2d00c36f9c8d06a21a9e4dcec5c45127f913
rldnrl <devpark1002@gmail.com>
2022-05-20 22:08:45 +0900

    feat: button 컴포넌트 사용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import helloWorld from "./hello-world";
 import addImage from './add-image'
+import Button from "./components/Button";
+
+const button = new Button()
+button.render()
 
 helloWorld()
 addImage()
