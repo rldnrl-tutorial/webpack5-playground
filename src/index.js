429b52cffcd34dfee5df7c9fbcb6d7fdd737f3e9
rldnrl <devpark1002@gmail.com>
2022-05-21 11:56:38 +0900

    feat: heading 컴포넌트 사용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import helloWorld from "./hello-world";
 import addImage from './add-image'
 import Button from "./components/Button";
+import Heading from "./components/Heading/Heading";
 
 const button = new Button()
 button.render()
+
+const heading = new Heading()
+heading.render()
 
 helloWorld()
 addImage()
