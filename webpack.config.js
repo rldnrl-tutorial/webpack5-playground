57a795ca00f925e502d67dea01ad328348d64bc1
rldnrl <devpark1002@gmail.com>
2022-05-21 12:06:58 +0900

    feat: babel 설정

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,17 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
     ],
   },
   mode: 'none'
