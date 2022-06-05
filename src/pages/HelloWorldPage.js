ae3ddde36c871269f462202ddbb63eb5d876616b
rldnrl <devpark1002@gmail.com>
2022-06-05 20:30:45 +0900

    feat: add parameter

--- src/pages/HelloWorldPage.js
+++ src/pages/HelloWorldPage.js
@@ -2,15 +2,16 @@ import helloWorld from "../hello-world";
 import addImage from '../add-image'
 import Button from "../components/Button";
 import Heading from "../components/Heading/Heading";
+import upperCase from 'lodash/upperCase'
 
 const button = new Button()
 button.render()
 
 const heading = new Heading()
-heading.render()
+heading.render(upperCase('Hello World'))
 
 const heading2 = new Heading()
-heading2.render()
+heading2.render(upperCase('hello world'))
 
 helloWorld()
 addImage()
