817a73eb7f50a6c7766d257aa423af667786aaa4
rldnrl <devpark1002@gmail.com>
2022-06-05 20:30:34 +0900

    feat: add parameter

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,10 +1,10 @@
 import './style.scss'
 
 class Heading {
-  render() {
+  render(pageName) {
     const headingElement = document.createElement('h1')
     headingElement.className = 'xl px-1 py-2 text-green-500'
-    headingElement.innerHTML = 'This is Heading Tag'
+    headingElement.innerHTML = `This is Heading Tag ${pageName} page`
     const body = document.querySelector('body')
     body.appendChild(headingElement)
   }
